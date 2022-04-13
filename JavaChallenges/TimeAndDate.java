0e57efc5b7aa385fc178ac4b4d0e4edef446a4be
DivyaMaheswari <84456364+DivyaMaheswari@users.noreply.github.com>
2022-04-13 22:15:35 +0530

    Add files via upload

---
+++ JavaChallenges/TimeAndDate.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Date;
+
+public class TimeAndDate {
+    public static void main(String[] args) {
+        Date current_date = new Date();
+        System.out.println(current_date);
+    }
+}
