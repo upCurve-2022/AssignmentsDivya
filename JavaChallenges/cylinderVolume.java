2290750c01787ff61702d13da31e2bd0e97e503b
DivyaMaheswari <84456364+DivyaMaheswari@users.noreply.github.com>
2022-04-13 22:38:00 +0530

    Add files via upload

---
+++ JavaChallenges/cylinderVolume.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class cylinderVolume {
+    public static void main(String[] args) {
+        int radius = 11;
+        int height = 6;
+        double pi = 3.1428;
+        double volume = pi*(radius*radius)*height;
+        System.out.print(volume);
+    }
+}
